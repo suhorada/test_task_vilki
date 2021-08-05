e6e30d724c9a59c8579b785b605594ee6a07c8bd
Semyon Suhorada <s.suhorada@dunice.net>
2021-08-05 18:00:55 +0300

    try to do queue

--- back/services/subscribe.js
+++ back/services/subscribe.js
@@ -1,6 +1,6 @@
-const tasks = [];
+const tasks = [1, 2, 3, 4, 5, 6, 7];
 
-function recieve() {
+function receive() {
   tasks.push('a');
   console.log(tasks);
 }
@@ -9,13 +9,40 @@ function send() {
   console.log(tasks.shift());
 }
 
-exports = { recieve, send };
+function sender() {
+  let run = true;
+  setInterval(() => {
+    if (run) {
+      send();
+      if (!tasks.length) {
+        run = false;
+      }
+    }
+  }, 1000);
+}
 
-// var keepGoing = true;
-// setInterval(function () {
-//      if (keepGoing) {
-//         //DO YOUR STUFF HERE
-//         console.log(i);
-//      }
-//      //YOU CAN CHANGE 'keepGoing' HERE
-//   }, 500);
+// one receive with users array
+// receive();
+// receive();
+// receive();
+// receive();
+// one receive with users array
+// sender();
+
+const queue = (type, array) => new Promise((res) => setTimeout(() => {
+  const requests = [];
+  while (array.length) {
+    setTimeout(() => {
+      const temp = array.shift();
+      requests.push(new Promise((resolve) => {
+        console.log(type, ' to ', temp);
+        resolve();
+      }));
+    }, 1000);
+  }
+  res();
+}, 500));
+
+queue('asd', tasks);
+
+exports = { tasks, receive, send };
